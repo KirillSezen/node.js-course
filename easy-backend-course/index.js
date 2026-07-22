1da7efb3a0b6c3d348bce677c7ece963482a3a02
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-22 23:55:47 +0300

    add first request

--- easy-backend-course/index.js
+++ easy-backend-course/index.js
@@ -4,4 +4,8 @@ const PORT = 5000
 
 const app = express()
 
+app.get('/', (req, res) => {
+	res.status(200).json({text: 'сервер работает'})
+})
+
 app.listen(PORT, () => console.log(`server started on: ${PORT}`))
