fd03c4d9d0adfbe67fbaaf902d4e3ac438e906fe
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-22 03:06:21 +0300

    using mongo

--- main-course/server.js
+++ main-course/server.js
@@ -3,6 +3,9 @@ const Application = require('./framework/Application')
 const userRouter = require('./src/user-router')
 const jsonParser = require('./framework/parseJson')
 const urlParser = require('./framework/parseUrl')
+const mongoose = require('mongoose');
+const { setServers } = require('node:dns/promises')
+setServers(["1.1.1.1", "8.8.8.8"])
 
 dotenv.config()
 const PORT = process.env.PORT || 3000
@@ -14,6 +17,21 @@ app.use(urlParser('http://localhost:8800'))
 
 app.addRouter(userRouter)
 
-app.listen(PORT, () => {
-	console.log(`Server started on Port: ${PORT}`)
-})
+	
+
+const uri = process.env.URI
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+const start = async () => {
+	try {
+ 		await mongoose.connect(uri, clientOptions);
+		app.listen(PORT, () => {
+		console.log(`Server started on Port: ${PORT}`)
+	})
+	} catch (e) {
+		console.log(e)
+	}
+
+}
+
+start();
--- main-course/src/user-controller.js
+++ main-course/src/user-controller.js
@@ -1,20 +1,17 @@
-const users = [
-	{id: 1, name: 'Kirill Sezen'},
-	{id: 2, name: 'Ivan'},
-	{id: 3, name: 'Sanya'}
-]
+const User = require('./user-model')
 
-const getUsers = (req, res) => {
+const getUsers = async (req, res) => {
+	let users
 	if(req.params.id) {
-		return res.send(users.find(user => user.id == req.params.id))
+		users = await User.findById(req.params.id)
+	} else {
+		users = await User.find()
 	}
-
 	res.send(users)
 }
 
-const createUser = (req, res) => {
-	const user = req.body
-	users.push(user)
+const createUser = async (req, res) => {
+	const user = await User.create(req.body)
 	res.send(user)
 }
 
