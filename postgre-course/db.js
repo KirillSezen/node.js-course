d217741f8437acce1606427b04ac03bd9920a657
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-24 23:56:08 +0300

    connecting to db

---
+++ postgre-course/db.js
@@ -0,0 +1,12 @@
+const Pool = require('pg').Pool
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const pool = new Pool({
+	user: process.env.PG_USER,
+	password: process.env.PG_PASSWORD,
+	host: process.env.PG_HOST,
+	port: process.env.PG_PORT,
+	database: process.env.DB_NAME
+})
--- postgre-course/index.js
+++ postgre-course/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Hello Postgres + Node.js')
+})
 
 app.listen(PORT, () => console.log(`Сервер работает на: ${PORT}`))
