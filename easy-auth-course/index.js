7cd18e4c16662594590a230302bbd5fd0dffd60c
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-25 17:32:16 +0300

    easy-auth initial commit

---
+++ easy-auth-course/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { setServers } = require('node:dns/promises')
+setServers(["1.1.1.1", "8.8.8.8"])
+
+
+const PORT = process.env.PORT || 3000
+const DB_URL = process.env.DB_URL
+
+const app = express()
+
+app.use(express.json())
+
+const start = async () => {
+	try {
+		await mongoose.connect(DB_URL)
+		app.listen(PORT, () => console.log(`Server started at ${PORT}`))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()
