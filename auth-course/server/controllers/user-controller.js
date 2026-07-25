2f35a0614eb58c22780e80bacc8e9a3b9f340bae
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-26 00:52:04 +0300

    auth-course initial commit

---
+++ auth-course/server/controllers/user-controller.js
@@ -0,0 +1,27 @@
+class UserController {
+	registration (req, res) {
+		res.send('ok')
+	}
+
+	login (req, res) {
+		res.send('ok')
+	}
+
+	logout (req, res) {
+		res.send('ok')
+	}
+
+	activate (req, res) {
+		res.send('ok')
+	}
+
+	refresh (req, res) {
+		res.send('ok')
+	}
+
+	getUsers (req, res) {
+		res.send('ok')
+	}
+}
+
+module.exports = new UserController()
---
+++ auth-course/server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const router = require('./router/index')
+const { setServers } = require('node:dns/promises')
+setServers(["1.1.1.1", "8.8.8.8"])
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use('/auth', router)
+
+const start = async () => {
+	try {
+		await mongoose.connect(process.env.DB_URL)
+		app.listen(PORT, () => console.log(`Server started at ${PORT}`))
+	} catch(e) {
+		console.log(e)
+	}
+}
+
+start()
