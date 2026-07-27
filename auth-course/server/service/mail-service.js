411ec698b31148f682d885ad9645672071fe4644
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-27 15:32:29 +0300

    add mail-service

--- auth-course/server/service/mail-service.js
+++ auth-course/server/service/mail-service.js
@@ -1,8 +1,32 @@
-
+const nodemailer = require('nodemailer')
 
 class MailService {
-	async sendActivationMail(to, link) {
 
+	constructor() {
+		this.transporter = nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				user: process.env.MAIL_USER,
+				pass: process.env.MAIL_PASSWORD
+			},
+			tls: {
+				rejectUnauthorized: false
+			}
+		})
+	}
+
+	async sendActivationMail(to, link) {
+		await this.transporter.sendMail({
+			from: process.env.MAIL_USER,
+			to,
+			subject: 'Активация аккаунта на ' + process.env.API_URL,
+			text: '',
+			html:` 
+				<div>
+					<h1>Для активации перейдите по ссылке</h1>
+					<a href="${link}">${link}</a>
+				</div>`
+		})
 	}
 }
 
